f8d6c563bd5398136cbacc631f3adc415bb575bd
meme <memescape3@gmail.com>
2023-11-22 20:18:53 +0530

    gc

--- src/instituteSettings/repository.ts
+++ src/instituteSettings/repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { InstituteSetting } from './schema';
-import { BaseRepository } from 'src/search/base.repository';
+import { BaseRepository } from 'base.repository';
 
 @Injectable()
 export class InstituteSettingRepository extends BaseRepository<InstituteSetting> {
--- src/search/repository.ts
+++ src/search/repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { BaseRepository } from 'src/search/base.repository';
+import { BaseRepository } from 'base.repository';
 import { BookInstitute } from 'src/book-institute-relation/schema';
 import { Book } from 'src/books/schema';
 
